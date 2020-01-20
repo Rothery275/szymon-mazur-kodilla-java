e5105fd09bf3d169076592e659f90adbc752f856
Rothery275 <szymon96mazur@gmail.com>
2020-01-20 19:07:00 +0100

    Calculator test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,34 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        //next test
+        Calculator calculator = new Calculator();
+        double addingResult = calculator.addAToB(19.51, 56.86);
+        double subtractingResult = calculator.subtractAFromB(754.49,157.12);
+
+        if(addingResult == 19.51 + 56.86) {
+            System.out.println("Success! Result equals: " + addingResult);
+        } else {
+            System.out.println("Error of adding!");
+        }
+
+        if(subtractingResult == 754.49 - 157.12) {
+            System.out.println("Success! Result equals: " + subtractingResult);
+        } else {
+            System.out.println("Error of subtracting!");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.calculator;
+
+import com.kodilla.testing.TestingMain;
+
+public class Calculator {
+
+    public double addAToB(double a, double b) {
+        return a + b;
+    }
+    public double subtractAFromB(double a, double b) {
+        return a - b;
+    }
+}
