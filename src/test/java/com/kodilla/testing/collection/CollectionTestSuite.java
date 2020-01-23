16ee1fd5e3a9633f4eb349b9bd7eeed4f2d9b65f
Rothery275 <szymon96mazur@gmail.com>
2020-01-23 18:23:38 +0100

    6.3

--- src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -31,18 +31,18 @@ public class CollectionTestSuite {
     public void testOddNumbersExterminatorNormalList() {
         //Given
         ArrayList<Integer> numbers = new ArrayList<Integer>();
-        numbers.add(0,3);
-        numbers.add(1,5);
-        numbers.add(2,4);
-        numbers.add(3,10);
-        numbers.add(4,15);
-        numbers.add(5,20);
+        numbers.add(0, 3);
+        numbers.add(1, 5);
+        numbers.add(2, 4);
+        numbers.add(3, 10);
+        numbers.add(4, 15);
+        numbers.add(5, 20);
         OddNumbersExterminator exterminator = new OddNumbersExterminator();
-        //When
         ArrayList<Integer> actualList = new ArrayList<>();
         actualList.add(4);
         actualList.add(10);
         actualList.add(20);
+        //When
         ArrayList<Integer> testList = exterminator.exterminate(numbers);
         //Then
         Assert.assertEquals(actualList, testList);
